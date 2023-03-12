307c64be2d8e8baa9618fb0a253908d75806c6a2
knamroud <87078943+knamroud@users.noreply.github.com>
2023-03-12 17:30:08 +0100

    Added option to add additional flags to pass to the tsc.

--- tsccthread.py
+++ tsccthread.py
@@ -7,11 +7,12 @@ from os.path import exists, join, getmtime
 class TSCCThread(Thread):
     '''Thread used to check and compile TypeScript files in your Python Flask/Django project'''
 
-    def __init__(self, tspath: str, jspath: str, debug: bool = False):
+    def __init__(self, tspath: str, jspath: str, flags: list = [], debug: bool = False):
         Thread.__init__(self)
-        self.tsp = tspath
-        self.jsp = jspath
-        self.debug = debug # Defaults to False, if True the thread will check files until stopped, otherwise only one time
+        self.tsp = tspath  # Path containing TypeScript files to compile.
+        self.jsp = jspath  # Path containing resulting JavaScript files.
+        self.flags = flags # Default to empty list, contains additional flags to pass to the tsc compiler.
+        self.debug = debug # Defaults to False, if True the thread will check files until stopped, otherwise only one time.
 
     def run(self):
         while True:
@@ -25,9 +26,10 @@ class TSCCThread(Thread):
                 ts = join(self.tsp, file)
                 if not exists(js) or getmtime(js) < getmtime(ts):
                     print(f" * Detected change in '{ts}', recompiling.")
-                    run(["tsc", ts, "--outFile", js, "--module", "amd"])
+                    run(["tsc", ts, "--outFile", js]+self.flags)
             for file in listdir(self.jsp):
                 if not exists(join(self.tsp, file.replace("js", "ts"))):
                     remove(join(self.jsp, file))
             if not self.debug:
                 break
+                
